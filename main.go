7289346d7820e3c2f71dc0bbc658338b26df3990
qwe7002 <dev@nicoid.com>
2025-03-11 21:55:02 +0800

    fix: implement concurrent processing for Dig API requests

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 type DigResult struct {
@@ -26,47 +27,55 @@ type DigRequest struct {
 func dig(req DigRequest) DigResult {
 	var server = []string{"1.1.1.1", "8.8.8.8", "114.114.114.114", "2400:3200:baba::1", "2402:4e00::"}
 	var answers []Answer
+	var wg sync.WaitGroup
 	for _, s := range server {
-		cmd := exec.Command("dig", req.Domain, req.TypeOfRecord, "@"+s, "+noall", "+answer")
-		out, err := cmd.Output()
-		if err != nil {
-			answers = append(answers, Answer{
-				RequestServer: s,
-				Error:         err.Error(),
-			})
+		wg.Add(1)
+		go digCommand(&wg, &answers, req.Domain, s, req.TypeOfRecord)
+	}
+	wg.Wait()
+	result := DigResult{Answer: answers}
+	return result
+}
+
+func digCommand(wg *sync.WaitGroup, answers *[]Answer, domain string, server string, recordType string) {
+	defer wg.Done()
+	cmd := exec.Command("dig", domain, recordType, "@"+server, "+noall", "+answer")
+	out, err := cmd.Output()
+	if err != nil {
+		*answers = append(*answers, Answer{
+			RequestServer: server,
+			Error:         err.Error(),
+		})
+		return
+	}
+
+	lines := strings.Split(string(out), "\n")
+	haveAnswer := false
+	for _, line := range lines {
+		if strings.HasPrefix(line, ";") {
 			continue
 		}
-
-		lines := strings.Split(string(out), "\n")
-		haveAnswer := false
-		for _, line := range lines {
-			if strings.HasPrefix(line, ";") {
+		if line != "" {
+			fields := strings.Fields(line)
+			if len(fields) >= 5 {
+				haveAnswer = true
+				*answers = append(*answers, Answer{
+					IP:            fields[4],
+					RecordType:    fields[3],
+					TTL:           fields[1],
+					RequestServer: server,
+				})
 				continue
 			}
-			if line != "" {
-				fields := strings.Fields(line)
-				if len(fields) >= 5 {
-					haveAnswer = true
-					answers = append(answers, Answer{
-						IP:            fields[4],
-						RecordType:    fields[3],
-						TTL:           fields[1],
-						RequestServer: s,
-					})
-					continue
-				}
-			} else {
-				if !haveAnswer {
-					answers = append(answers, Answer{
-						RequestServer: s,
-						Error:         "No answer",
-					})
-				}
+		} else {
+			if !haveAnswer {
+				*answers = append(*answers, Answer{
+					RequestServer: server,
+					Error:         "No answer",
+				})
 			}
 		}
 	}
-	result := DigResult{Answer: answers}
-	return result
 }
 
 func handler(c *gin.Context) {
