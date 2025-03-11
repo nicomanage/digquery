c202f3b8293f867c8c4b0a7b4d6ebc07c7628993
qwe7002 <dev@nicoid.com>
2025-03-11 22:42:24 +0800

    fix: refactor Dig API response structure to use a map for answers

--- main.go
+++ main.go
@@ -8,15 +8,14 @@ import (
 )
 
 type DigResult struct {
-	Answer []Answer `json:"answer"`
+	Answer map[string][]Answer `json:"answer"`
 }
 
 type Answer struct {
-	IP            string `json:"ip,omitempty"`
-	TTL           string `json:"ttl,omitempty"`
-	RecordType    string `json:"record_type,omitempty"`
-	RequestServer string `json:"request_server,omitempty"`
-	Error         string `json:"error,omitempty"`
+	IP         string `json:"ip,omitempty"`
+	TTL        string `json:"ttl,omitempty"`
+	RecordType string `json:"record_type,omitempty"`
+	Error      string `json:"error,omitempty"`
 }
 
 type DigRequest struct {
@@ -26,26 +25,27 @@ type DigRequest struct {
 
 func dig(req DigRequest) DigResult {
 	var server = []string{"1.1.1.1", "8.8.8.8", "114.114.114.114", "2400:3200:baba::1", "2402:4e00::"}
-	var answers []Answer
+	answers := make(map[string][]Answer)
 	var wg sync.WaitGroup
 	for _, s := range server {
 		wg.Add(1)
 		go digCommand(&wg, &answers, req.Domain, s, req.TypeOfRecord)
 	}
 	wg.Wait()
-	result := DigResult{Answer: answers}
+	var result = DigResult{Answer: answers}
 	return result
 }
 
-func digCommand(wg *sync.WaitGroup, answers *[]Answer, domain string, server string, recordType string) {
+func digCommand(wg *sync.WaitGroup, answers *map[string][]Answer, domain string, server string, recordType string) {
 	defer wg.Done()
+	var ipList []Answer
 	cmd := exec.Command("dig", domain, recordType, "@"+server, "+noall", "+answer")
 	out, err := cmd.Output()
 	if err != nil {
-		*answers = append(*answers, Answer{
-			RequestServer: server,
-			Error:         err.Error(),
+		ipList = append(ipList, Answer{
+			Error: "error",
 		})
+		(*answers)[server] = ipList
 		return
 	}
 
@@ -59,22 +59,21 @@ func digCommand(wg *sync.WaitGroup, answers *[]Answer, domain string, server str
 			fields := strings.Fields(line)
 			if len(fields) >= 5 {
 				haveAnswer = true
-				*answers = append(*answers, Answer{
-					IP:            fields[4],
-					RecordType:    fields[3],
-					TTL:           fields[1],
-					RequestServer: server,
+				ipList = append(ipList, Answer{
+					IP:         fields[4],
+					RecordType: fields[3],
+					TTL:        fields[1],
 				})
 			}
 		} else {
 			if !haveAnswer {
-				*answers = append(*answers, Answer{
-					RequestServer: server,
-					Error:         "No answer",
+				ipList = append(ipList, Answer{
+					Error: "No answer",
 				})
 			}
 		}
 	}
+	(*answers)[server] = ipList
 }
 
 func handler(c *gin.Context) {
